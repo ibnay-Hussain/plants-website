86665927c3715b73ce8335f058eb36c46598c114
ibnay <zainishaaah@gmail.com>
2025-09-09 15:00:30 +0500

    react+tailwind setup

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <p className="text-green-600 px-5 text-[1.5rem]">
+          Welcome to plants website.
+        </p>
+      </div>
+    )
+  }
+}

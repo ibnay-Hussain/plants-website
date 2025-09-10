5b4bf4a60bce9ee2c1203c9d131d173e437b0383
ibnay <zainishaaah@gmail.com>
2025-09-10 18:34:45 +0500

    add styling & animation
    home page animation

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import './index.css';
 import Navbar from './components/navbar/Navbar';
+import Home from './components/home/Home';
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Home />
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,24 @@ module.exports = {
         jost: ['Jost', 'sans-serif'],
         lobster: ['Lobster', 'cursive'],
       },
+      keyframes: {
+        move: {
+          '50%': { transform: 'translateY(-1rem)' },
+        },
+        rotate: {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(360deg)' },
+        },
+        scale: {
+          '0%, 100%': { transform: 'scale(1)' },
+          '50%': { transform: 'scale(1.1)' },
+        }
+      },
+      animation: {
+        movingY: 'move 3s linear infinite',
+        rotating: 'rotate 20s linear infinite',
+        scaling: 'scale 3s ease-in-out infinite',
+      }
     },
   },
   plugins: [],
